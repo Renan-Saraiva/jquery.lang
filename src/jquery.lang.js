67f1c308214e2f5a2a3eea7d62fea71b8e9d450f
Renan Magalhães Saraiva <renan.m.saraiva@gmail.com>
2019-02-18 11:40:56 -0300

    Development of the project base code.

---
+++ src/jquery.lang.js
@@ -0,0 +1,83 @@
+﻿(function ($) {
+
+    var lang = function (dictionary, options) {
+
+        this.run = function () {
+
+            var $this = this;
+
+            $("[" + this.options.dataAttributeToken + "]").each(function (index) {
+                $this.setText($(this));
+            });
+
+            $("[" + this.options.dataAttributeTokenPlaceholder + "]").each(function (index) {
+                $this.setPlaceholder($(this));
+            });
+
+            $("[" + this.options.dataAttributeTokenTitle + "]").each(function (index) {
+                $this.setTitle($(this));
+            });
+        }
+
+        this.setText = function ($element) {
+            var token = $element.attr(this.options.dataAttributeToken);
+            $element.text(this.getTokenValue(token));
+        }
+
+        this.setPlaceholder = function ($element) {
+            var token = $element.attr(this.options.dataAttributeTokenPlaceholder);
+            $element.attr('placeholder', this.getTokenValue(token));
+        }
+
+        this.setTitle = function ($element) {
+            var token = $element.attr(this.options.dataAttributeTokenTitle);
+            $element.attr('title', this.getTokenValue(token));
+        }
+
+        this.change = function (lang) {
+            this.options.currentLang = lang;
+            
+            if (typeof (Storage) !== "undefined") {
+                localStorage.setItem("jqueryLangChub", lang);
+            }
+
+            this.run();
+        }
+
+        this.getTokenValue = function (token, lang) {
+            return this.dictionary[lang || this.options.currentLang][token];
+        }
+
+        this.defaults = {
+            currentLang: "pt-br",
+            dataAttributeToken: "data-lang-token",
+            dataAttributeTokenPlaceholder: "data-lang-placeholder",
+            dataAttributeTokenTitle: "data-lang-title"
+        };
+
+        this.options = $.extend({}, this.defaults, options);
+
+        this.dictionary = dictionary;
+
+        if (typeof (Storage) !== "undefined")
+        {
+            if (options !== undefined && options.currentLang !== undefined) {
+                localStorage.setItem("jqueryLangChub", this.options.currentLang);
+            }
+            else
+            {
+                if (localStorage.jqueryLangChub === undefined)
+                    localStorage.setItem("jqueryLangChub", this.options.currentLang);
+                else
+                    this.options.currentLang = localStorage.jqueryLangChub;
+            }                
+        }
+
+        this.run();
+
+        return this;
+    };
+
+    $.lang = lang;
+
+}(jQuery));
